fbb658f2e47123d561858494fa31a3e37cff5295
Kevin1875 <94397214+Kevin1875@users.noreply.github.com>
2022-03-28 15:34:24 -0500

    Add files via upload

---
+++ televisores/control.py
@@ -0,0 +1,45 @@
+from televisores.TV import TV
+
+class Control:
+    
+    def __init__(self):
+        self._tv = ""
+      
+    def turnOn(self):
+      TV.turnOn(self)
+      
+
+    def turnOff(self):
+      TV.turnOff(self)
+      
+      
+    def canalUp(self):
+      TV.canalUp(self)
+    
+    def canalDown(self):
+      TV.canalDown(self)
+
+    def volumenUp(self):
+      TV.volumenUp(self)
+
+    def volumenDown(self):
+      TV.volumenDown(self)
+
+    def setCanal(self, canal):
+      TV.setCanal(self, canal)
+      
+
+    def enlazar(self, tv):
+      self._tv = tv
+      TV.setControl(self, Control)
+      
+
+    def setTv(self, tv):
+      self._tv= tv
+    
+    def getTv(self):
+      return self._tv
+      
+    
+  
+    
---
+++ televisores/marca.py
@@ -0,0 +1,11 @@
+class Marca:
+    
+    def __init__(self, name):
+        self._name = name
+      
+
+    def setNombre(self, name):
+        self._name = name
+
+    def getNombre(self):
+      return self._name
